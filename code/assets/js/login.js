e873c20d37f3fc36acf0ba25ff450a9178f98930
tiewenhui <tiewenhui@qq.com>
2020-06-20 10:56:47 +0800

    第一次提交

--- code/assets/js/login.js
+++ code/assets/js/login.js
@@ -1,11 +1,11 @@
 $(function () {
-  // 点击‘去注册账号’链接
+  //点击去注册账号的链接
   $('#link_reg').on('click', function () {
     $('.login-box').hide()
     $('.reg-box').show()
   })
-  //点击去‘登录’链接
-  $('#link_reg').on('click', function () {
+  //点击去登录的链接
+  $('#link_login').on('click', function () {
     $('.login-box').show()
     $('.reg-box').hide()
   })
